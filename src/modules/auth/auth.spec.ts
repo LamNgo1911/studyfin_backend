a5c1b68bffbc4a4340bd00a3c92eed69a6819b63
liam <lamngo606@gmail.com>
2026-03-19 23:42:58 +0200

    create files for auth

---
+++ src/modules/auth/auth.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+describe('AuthModule', () => {
+  let controller: AuthController;
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+});
