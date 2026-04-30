a57a78bca0fc86e0cc306d785a6b41df9df58ee5
liam <lamngo606@gmail.com>
2026-05-01 00:20:12 +0300

    fix(03): register RolesGuard and Reflector in GuidanceModule providers (CR-01)

--- src/modules/guidance/guidance.module.ts
+++ src/modules/guidance/guidance.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GuidanceController } from './guidance.controller';
 import { GuidanceService } from './guidance.service';
+import { RolesGuard } from '../../common/guards/roles.guard';
 
 @Module({
   imports: [],
   controllers: [GuidanceController],
-  providers: [GuidanceService],
+  providers: [GuidanceService, RolesGuard, Reflector],
   exports: [GuidanceService],
 })
 export class GuidanceModule {}
